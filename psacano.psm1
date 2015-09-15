80cc7666d7a2a9f1fd6c7feefd45101efb58673a
Tom-Inge Larsen <tom@codesalot.com>
2015-09-15 21:03:09 +0200

    Added function for setting general parameters for the session and general function for the GET commands

---
+++ psacano.psm1
@@ -0,0 +1,35 @@
+﻿function Acano-GET {
+    Param (
+        [parameter(Mandatory=$true)]
+        [string]$NodeLocation
+    )
+
+    $webclient = New-Object System.Net.WebClient
+    $credCache = new-object System.Net.CredentialCache
+    $credCache.Add($script:APIAddress, "Basic", $script:creds)
+
+    $webclient.Headers.Add(“user-agent”, “Windows Powershell WebClient”)
+    $webclient.Credentials = $credCache
+
+    [xml]$doc = $webclient.DownloadString($script:APIAddress+$NodeLocation)
+
+    return $doc
+}
+
+function New-AcanoSession {
+    Param (
+        [parameter(Mandatory=$true)]
+        [string]$APIAddress,
+        [string]$Port = $null,
+        [parameter(Mandatory=$true)]
+        [PSCredential]$Credential
+    )
+
+    if ($Port -ne $null){
+        $script:APIAddress = "https://"+$APIAddress+":"+$Port+"/"
+    } else {
+        $script:APIAddress = "https://"+$APIAddress+"/"
+    }
+
+    $script:creds = $Credential
+}
