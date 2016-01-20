1e81cacc12b899d22f90904c3d457b38d6ae95f4
Tom-Inge Larsen <tom@codesalot.com>
2016-01-20 23:20:20 +0100

    beginnings of Get-AcanoIpv4

--- PsAcano-MMP.psm1
+++ PsAcano-MMP.psm1
@@ -87,12 +87,12 @@ Function Get-AcanoIface {
         $ifaceobj = New-Object System.Object
         $sshresult = (Invoke-SSHCommand -Command "iface $iface" -SessionId $script:SSHSessionId).Output
 
-        $macadress = ($sshresult | Select-String -Pattern "Mac address" -SimpleMatch).ToString()
-        $autoneg = ($sshresult | Select-String -Pattern "Auto-Negotiation" -SimpleMatch).ToString()
+        $macadress = ($sshresult | Select-String -Pattern "Mac address" -SimpleMatch).Line
+        $autoneg = ($sshresult | Select-String -Pattern "Auto-Negotiation" -SimpleMatch).Line
         
         if ($sshresult | Select-String -Pattern "Speed" -SimpleMatch -Quiet) {
-            $speed = ($sshresult | Select-String -Pattern "Speed" -SimpleMatch).ToString()
-            $duplex = ($sshresult | Select-String -Pattern "Duplex" -SimpleMatch).ToString()
+            $speed = ($sshresult | Select-String -Pattern "Speed" -SimpleMatch).Line
+            $duplex = ($sshresult | Select-String -Pattern "Duplex" -SimpleMatch).Line
         }
         else {
             ## Speed and duplex null
@@ -100,7 +100,7 @@ Function Get-AcanoIface {
             $duplex = ""
         }
         
-        $MTU = ($sshresult | Select-String -Pattern "MTU" -SimpleMatch).ToString()
+        $MTU = ($sshresult | Select-String -Pattern "MTU" -SimpleMatch).Line
 
         $ifaceobj | Add-Member -MemberType NoteProperty -Name Identity -Value $iface
         $ifaceobj | Add-Member -MemberType NoteProperty -Name macaddress -Value $macadress.Substring($macadress.Length-17)
@@ -128,4 +128,53 @@ function Set-AcanoIfaceMTU {
 
     Get-AcanoIface $Identity
 
+}
+
+function Get-AcanoIpv4 {
+    Param (
+        [parameter(Mandatory=$false,Position=1)]
+        [string]$Identity
+    )
+
+    If (($Identity -eq $null) -or ($Identity -eq "")) {
+        $ifaces = Get-AcanoIfaceList
+    }
+    else {
+        $ifaces = @($Identity)
+    }
+
+    $ipv4output = @()
+
+    foreach ($iface in $ifaces) {
+        $sshresult = (Invoke-SSHCommand -Command "ipv4 $iface" -SessionId $script:SSHSessionId).Output
+        $ipv4obj = New-Object System.Object
+
+        $ipv4obj | Add-Member -MemberType NoteProperty -Name Identity -Value $iface
+
+        if (-not ($sshresult | Select-String -Pattern "No observed values for interface b" -quiet)) {
+            $ipaddress = ($sshresult | Select-String -Pattern "address" -SimpleMatch).Line[0]
+            $prefixlength = ($sshresult | Select-String -Pattern "prefixlen" -SimpleMatch).Line
+            $gateway = ($sshresult | Select-String -Pattern "gateway" -SimpleMatch).Line[0]
+            $macadress = ($sshresult | Select-String -Pattern "macaddress" -SimpleMatch).Line
+            $default = ($sshresult | Select-String -Pattern "default" -SimpleMatch).Line        
+            $dhcp = ($sshresult | Select-String -Pattern "dhcp" -SimpleMatch).Line
+            $enabled = ($sshresult | Select-String -Pattern "enabled" -SimpleMatch).Line
+
+            $ipv4obj | Add-Member -MemberType NoteProperty -Name IpAddress -Value $ipaddress.Substring($ipaddress.LastIndexOf(' ')+1)
+            $ipv4obj | Add-Member -MemberType NoteProperty -Name Gateway -Value $gateway.Substring($gateway.LastIndexOf(' ')+1)
+            $ipv4obj | Add-Member -MemberType NoteProperty -Name PrefixLength -Value $prefixlength.Substring($prefixlength.LastIndexOf(' ')+1)
+            $ipv4obj | Add-Member -MemberType NoteProperty -Name Macaddress -Value $macadress.Substring($macadress.LastIndexOf(' ')+1)
+            $ipv4obj | Add-Member -MemberType NoteProperty -Name default -Value $default.Substring($default.LastIndexOf(' ')+1)
+            $ipv4obj | Add-Member -MemberType NoteProperty -Name dhcp -Value $dhcp.Substring($dhcp.LastIndexOf(' ')+1)
+            $ipv4obj | Add-Member -MemberType NoteProperty -Name enabled -Value $enabled.Substring($enabled.LastIndexOf(' ')+1)
+        }
+        else {
+            ## Interface has dhcp set and hasn't recieved an address. This is lazy and should be fixed at some point, nonetheless:
+            $ipv4obj | Add-Member -MemberType NoteProperty -Name IpAddress -Value "Ip address not observed"
+        }
+              
+        $ipv4output += $ipv4obj
+    }
+
+    return $ipv4output
 }
